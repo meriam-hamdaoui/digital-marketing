1a0653b67a01ec516ec990aa63d74c3495fed464
meriam <meriam.hamdaoui@gmail.com>
2024-05-22 13:04:01 +0100

    footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,55 +1,60 @@
 import React from "react";
 import "./footer.css";
 import trendyAgency from "../../assets/img/trendy-agency.jpg";
-import { FloatingLabel, Form, Button } from "react-bootstrap";
+import { Form, Button } from "react-bootstrap";
 
 const Footer = () => {
   return (
     <footer id="footer">
-      <div className="top-footer d-flex">
-        <div>
+      <div className="top-footer d-flex justify-content-around">
+        <div className="footer-info  p-3">
           <img
             src={trendyAgency}
             alt="trendy-agency"
-            width={150}
-            height={150}
+            width={"35%"}
+            height={"35%"}
           />
-          <div>
-            <div>trendy agency</div>
-            <div>
-              Agence marketing digital spécialisée dans le réseaux sociaux,
+
+          <div className="footer-info-txt paragraph-font">
+            <p className="trendy-agency-font text-center">trendy agency</p>
+            <p className="shrink-text ">
+              Agence marketing digital spécialisée dans les réseaux sociaux,
               offrant une gamme complète de services pour aider les entreprises
               à tirer le meilleur parti de leur présence en ligne.
-            </div>
-          </div>
-        </div>
-        <div>
-          <div>Nos Contacts</div>
-          <div>
+            </p>
             <p>(+216) 21 95 60 39 / (+216) 28 14 90 95</p>
             <p>trendyagency@outlook.fr</p>
             <p>CENTRE URBAIN NORD, Tunis</p>
+            <p>insta + facebook</p>
           </div>
         </div>
-        <div>
-          <FloatingLabel
-            controlId="floatingTextarea"
-            label="Comments"
-            className="mb-3"
-          >
-            <Form.Control as="textarea" placeholder="Sujet" />
-          </FloatingLabel>
-          <FloatingLabel controlId="floatingTextarea2" label="Comments">
-            <Form.Control
-              as="textarea"
-              placeholder="Message"
-              style={{ height: "100px" }}
-            />
-          </FloatingLabel>
-          <Button>Envoyer</Button>
+        <div className="footer-form">
+          <div className="trendy-agency-font text-left">
+            Pour Plus d'information :
+          </div>
+          <Form>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Sujet</Form.Label>
+              <Form.Control type="text" placeholder="CC" />
+            </Form.Group>
+
+            <Form.Group
+              className="mb-3"
+              controlId="exampleForm.ControlTextarea1"
+            >
+              <Form.Label>Votre Message</Form.Label>
+              <Form.Control as="textarea" rows={3} />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Envoyer
+            </Button>
+          </Form>
         </div>
       </div>
-      <div className="bottom-footer">TRENDY AGENCY@copyright-2024</div>
+      <hr style={{ color: "rgba(207, 117, 153, 0.75)" }} />
+      <div className="bottom-footer text-center">
+        TRENDY AGENCY@copyright-2024
+      </div>
     </footer>
   );
 };
