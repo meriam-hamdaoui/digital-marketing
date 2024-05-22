59c55045276c4ea31d71a583295c605e2c692752
meriam <meriam.hamdaoui@gmail.com>
2024-05-22 14:29:55 +0100

    footer complete

--- src/components/footer/FooterForm.jsx
+++ src/components/footer/FooterForm.jsx
@@ -4,20 +4,23 @@ import { Form, Button } from "react-bootstrap";
 const FooterForm = () => {
   return (
     <div className="footer-form">
-      <div className="trendy-agency-font text-left">
+      <div
+        className="trendy-agency-font text-left"
+        style={{ padding: "15px 30px 20px" }}
+      >
         Pour Plus d'information :
       </div>
-      <Form>
+      <Form style={{ paddingLeft: "20px" }}>
         <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Sujet</Form.Label>
-          <Form.Control type="text" placeholder="CC" />
+          <Form.Label className="paragraph-font">Sujet</Form.Label>
+          <Form.Control type="text" placeholder="sujet" />
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-          <Form.Label>Votre Message</Form.Label>
-          <Form.Control as="textarea" rows={3} />
+          <Form.Label className="paragraph-font">Votre Message</Form.Label>
+          <Form.Control as="textarea" rows={3} placeholder="votre message" />
         </Form.Group>
-        <Button variant="primary" type="submit">
+        <Button type="button" id="envoyer">
           Envoyer
         </Button>
       </Form>
