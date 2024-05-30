0c5dfeb51cc070d0928be3ab94843af79fce8eb5
meriam <meriam.hamdaoui@gmail.com>
2024-05-30 15:05:31 +0100

    service section data et style

--- src/components/nosServices/ServiceCard.jsx
+++ src/components/nosServices/ServiceCard.jsx
@@ -1,16 +1,21 @@
 import React from "react";
 
 const ServiceCard = ({ service }) => {
-  const { title, img, desc } = service;
+  const { title, img, style, desc } = service;
 
   return (
     <div className="card">
       <div className="card-side front">
-        <img src={img} alt="" width={"60%"} height={"50%"} />
-        <div>{title}</div>
+        <img src={img} alt="service-img" style={style} />
+        <div
+          className="card-title text-center"
+          style={{ color: "white", lineHeight: "2rem" }}
+        >
+          {title}
+        </div>
       </div>
       <div className="card-side back">
-        <div>{desc}</div>
+        <div className="shrink-text card-p">{desc}</div>
       </div>
     </div>
   );
