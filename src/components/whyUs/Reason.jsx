42a393d81e42b870b115476bcad4fbf0672f4c9a
meriam <meriam.hamdaoui@gmail.com>
2024-05-30 16:06:48 +0100

    pourquoi section

--- src/components/whyUs/Reason.jsx
+++ src/components/whyUs/Reason.jsx
@@ -4,7 +4,7 @@ const Reason = ({ order, name, imgSrc, text }) => {
   return (
     <div className={`reason-card d-flex ${name}`}>
       <div className="img-container" style={{ order: order }}>
-        <img src={imgSrc} alt="reason" width={180} height={180} />
+        <img src={imgSrc} alt="reason" width={"180rem"} height={"180rem"} />
       </div>
       <div className="reason-text p-3">
         <p className="shrink-text paragraph-font">{text}</p>
