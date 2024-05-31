1d5a79ec24b3522f219200eb57ac49445da943c9
meriam <meriam.hamdaoui@gmail.com>
2024-05-31 16:39:07 +0100

    basic css done

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./footer.css";
-import FooterForm from "./FooterForm";
-import trendyAgency from "../../assets/img/trendy-agency.jpg";
+// import FooterForm from "./FooterForm";
+import trendyAgency from "../../assets/img/trendy-agency-removebg.png";
 import { MdOutlinePhoneIphone, MdOutlineMailOutline } from "react-icons/md";
 import { IoLocationOutline } from "react-icons/io5";
 import CustomSocialIcon from "./CustomSocialIcon";
@@ -9,52 +9,61 @@ import CustomSocialIcon from "./CustomSocialIcon";
 const Footer = () => {
   return (
     <footer id="footer">
-      <div className="top-footer d-flex justify-content-around">
-        <div className="footer-info  p-3">
+      <div className="top-footer">
+        {/* agency definition */}
+        <div className="footer-info">
           <img
             src={trendyAgency}
             alt="trendy-agency"
-            width={"40%"}
-            height={"40%"}
+            width={"30%"}
+            height={"30%"}
           />
 
-          <div className="footer-info-txt paragraph-font">
+          <div className="paragraph-font footer-intro">
             <p className="agency-name text-center">TRENDY AGENCY</p>
             <p className="shrink-text ">
               Agence marketing digital spécialisée dans les réseaux sociaux,
               offrant une gamme complète de services pour aider les entreprises
               à tirer le meilleur parti de leur présence en ligne.
             </p>
-            <p>
-              <MdOutlinePhoneIphone />
-              &nbsp;&nbsp; (+216) 21 95 60 39 / 28 14 90 95
-            </p>
-            <p>
-              <MdOutlineMailOutline />
-              &nbsp;&nbsp; trendyagency@outlook.fr
-            </p>
-            <p>
-              <IoLocationOutline />
-              &nbsp;&nbsp; CENTRE URBAIN NORD, Tunis
-            </p>
-            <div
-              className="d-flex justify-content-between"
-              style={{ width: "15%" }}
-            >
-              <CustomSocialIcon
-                url={"https://instagram.com/trendy.agency"}
-                size={25}
-              />
-              <CustomSocialIcon
-                url={
-                  "https://www.facebook.com/profile.php?id=61559745715549&mibextid=ZbWKwL"
-                }
-                size={25}
-              />
-            </div>
           </div>
         </div>
-        <FooterForm />
+
+        {/* contacts */}
+        <div className="paragraph-font">
+          <h4>Nos Contacts</h4>
+          <p>
+            <MdOutlinePhoneIphone />
+            &nbsp;&nbsp; (+216) 21 95 60 39 / 28 14 90 95
+          </p>
+          <p>
+            <MdOutlineMailOutline />
+            &nbsp;&nbsp; trendyagency@outlook.fr
+          </p>
+          <p>
+            <IoLocationOutline />
+            &nbsp;&nbsp; CENTRE URBAIN NORD, Tunis
+          </p>
+        </div>
+
+        {/* social media */}
+        <div className="paragraph-font">
+          <h4>Suivez-Nous</h4>
+          <div className="social-icons d-flex justify-content-around">
+            <CustomSocialIcon
+              url={"https://instagram.com/trendy.agency"}
+              size={25}
+            />
+            <CustomSocialIcon
+              url={
+                "https://www.facebook.com/profile.php?id=61559745715549&mibextid=ZbWKwL"
+              }
+              size={25}
+            />
+          </div>
+        </div>
+
+        {/* <FooterForm /> */}
       </div>
       <hr style={{ color: "rgba(207, 117, 153, 0.75)" }} />
       <div className="bottom-footer text-center">
