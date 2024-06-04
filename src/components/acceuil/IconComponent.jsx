ad13373c5b3f42a4e4bdad741a041f6a2df66ad1
meriam <meriam.hamdaoui@gmail.com>
2024-06-04 06:25:48 +0100

    resize social icons

--- src/components/acceuil/IconComponent.jsx
+++ src/components/acceuil/IconComponent.jsx
@@ -33,19 +33,19 @@ export const handleResize = () => {
   const icons = document.querySelectorAll(".icon");
   icons.forEach((icon) => {
     if (window.innerWidth <= 1200) {
-      icon.style.width = "35px";
-      icon.style.height = "35px";
+      icon.style.width = "7rem";
+      icon.style.height = "7rem";
     } else {
-      icon.style.width = "40px";
-      icon.style.height = "40px";
+      icon.style.width = "8rem";
+      icon.style.height = "8rem";
     }
   });
 };
 
 const IconComponent = ({ url }) => {
   const iconStyle = {
-    width: "30px", // Default width
-    height: "30px", // Default height
+    width: "5rem", // Default width
+    height: "5rem", // Default height
   };
 
   return <SocialIcon className="icon" url={url} style={iconStyle} />;
